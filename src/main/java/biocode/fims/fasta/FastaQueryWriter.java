12308d13a42811557ff917d2e087584aefa93571
RJ Ewing <ewing.rj@protonmail.com>
2018-03-02 16:09:29 -0800

    FastaQueryWriter now returns 1 file for each marker

--- src/main/java/biocode/fims/fasta/FastaQueryWriter.java
+++ src/main/java/biocode/fims/fasta/FastaQueryWriter.java
@@ -10,13 +10,11 @@ import biocode.fims.projectConfig.ProjectConfig;
 import biocode.fims.query.QueryResult;
 import biocode.fims.query.writers.QueryWriter;
 import biocode.fims.settings.PathManager;
-import org.apache.commons.lang.StringEscapeUtils;
+import biocode.fims.utils.FileUtils;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.*;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author RJ Ewing
@@ -24,6 +22,9 @@ import java.util.Set;
 public class FastaQueryWriter implements QueryWriter {
     private final QueryResult queryResult;
     private final ProjectConfig config;
+    private final Entity parentEntity;
+    private final String uniqueKey;
+    private final String parentUniqueKey;
 
     public FastaQueryWriter(QueryResult queryResult, ProjectConfig config) {
         // TODO pass in parentRecords & list of columns to write out as metadata
@@ -32,26 +33,69 @@ public class FastaQueryWriter implements QueryWriter {
             throw new FimsRuntimeException(FastaWriteCode.INVALID_ENTITY, 500, queryResult.entity().type());
         }
         this.queryResult = queryResult;
+
+        if (queryResult.get(false).size() == 0) {
+            throw new FimsRuntimeException(QueryCode.NO_RESOURCES, 400);
+        }
+
+        parentEntity = config.entity(queryResult.entity().getParentEntity());
+        uniqueKey = queryResult.entity().getUniqueKey();
+        parentUniqueKey = parentEntity.getUniqueKey();
     }
 
+    /**
+     * writes the records in QueryResults to a file. If more the 1 marker type is present,
+     * a zip file will be returned with a fasta file with records for each unique marker
+     *
+     * @return
+     */
     @Override
     public File write() {
-        File file = PathManager.createUniqueFile("output.fasta", System.getProperty("java.io.tmpdir"));
+        Map<String, List<Map<String, String>>> recordsMap = sortByMarker();
+        List<File> sequenceFiles = new ArrayList<>();
+
+        for (Map.Entry<String, List<Map<String, String>>> entry : recordsMap.entrySet()) {
+            sequenceFiles.add(
+                    writeMarkerFile(entry.getValue(), entry.getKey())
+            );
+        }
+
+        if (sequenceFiles.size() == 1) {
+            return sequenceFiles.get(0);
+        } else {
+            Map<String, File> fastaFileMap = new HashMap<>();
+
+            for (File fastaFile : sequenceFiles) {
+                fastaFileMap.put(fastaFile.getName(), fastaFile);
+            }
+
+            return FileUtils.zip(fastaFileMap, System.getProperty("java.io.tmpdir"));
+        }
+    }
+
+    private Map<String, List<Map<String, String>>> sortByMarker() {
+        Map<String, List<Map<String, String>>> fastaFileMap = new HashMap<>();
+
+        for (Map<String, String> record : queryResult.get(false)) {
+            String marker = record.get(FastaEntity.MARKER_KEY);
+
+            fastaFileMap
+                    .computeIfAbsent(marker, k -> new ArrayList<>())
+                    .add(record);
+        }
+
+        return fastaFileMap;
+    }
+
+    private File writeMarkerFile(List<Map<String, String>> records, String marker) {
+        String filename = StringUtils.isBlank(marker) ? "output.fasta" : marker + ".fasta";
+        File file = PathManager.createFile(filename, System.getProperty("java.io.tmpdir"));
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(file)))) {
 
-            List<Map<String, String>> records = queryResult.get(true);
 
-            if (records.size() == 0) {
-                throw new FimsRuntimeException(QueryCode.NO_RESOURCES, 400);
-            }
-
-            Entity parentEntity = config.entity(queryResult.entity().getParentEntity());
-
-            String uniqueKey = queryResult.entity().getUniqueKey();
-            String parentUniqueKey = parentEntity.getUniqueKey();
-            for (Map<String, String> record: records) {
+            for (Map<String, String> record : records) {
                 writer.write(">");
 
                 String identifier = queryResult.rootIdentifier();
